d7bba655153279a28e19f8b47cd5bd2263d1ea8e
Justin Stendara <jnstendara@gmail.com>
2023-04-27 19:30:08 -0500

    housekeeping

--- navbar/footer.js
+++ navbar/footer.js
@@ -7,7 +7,7 @@ export default function Footer (props) {
     <>
       <footer
         className="burntOrange text-center lg:text-left mt-20 max-h-max max-w-max">
-        <div className="mx-20 md: mx-80 py-10 text-center md:text-left">
+        <div className="mx-20 md:mx-80 py-10 text-center md:text-left">
           <div className="grid-1 grid gap-8 md:grid-cols-3 lg:grid-cols-3">
             <div className="">
               <h6
