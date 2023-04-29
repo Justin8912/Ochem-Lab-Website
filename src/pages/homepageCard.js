e0ea15b5bf8cab3e783bafffa6b18eb86c46f5e2
Justin Stendara <jnstendara@gmail.com>
2023-04-29 18:21:36 -0500

    Made a couple changes that Fjetland recommended - need to figure out the spacing of the boxes now that the description for the courses has gone away

--- src/pages/homepageCard.js
+++ src/pages/homepageCard.js
@@ -23,7 +23,7 @@ export default function HomepageCard (props) {
     <>
       <div className="max-w-md rounded overflow-hidden shadow-lg min-w-min mx-auto" style={{backgroundColor:'#efefef'}}>
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-3 text-center">{props.courseName}</div>
+          <div className="font-bold text-2xl mb-3 text-center">{props.courseName}</div>
           {
             props.imageLink &&
             <Image
