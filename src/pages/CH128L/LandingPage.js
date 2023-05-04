e6b875a0875cb4dbe30d799d222ee1bb7bba5cca
Justin Stendara <jnstendara@gmail.com>
2023-05-04 08:59:11 -0500

    Added initial file structure and initial blank rendering of the 128L page

---
+++ src/pages/CH128L/LandingPage.js
@@ -0,0 +1,25 @@
+import react from 'react';
+import Navbar from '../assets/navbar.js';
+import Footer from '../assets/footer.js';
+import LandingPage from '../assets/sectionLandingPage.js'
+import startInfo from '../../../resources/classInfo/CH128L/Start.txt';
+import relevantInfo from '../../../resources/classInfo/CH128L/RelevantInfo.txt'
+import readStartDateFile from '../../../resources/helperFunctions/readingStartDate.js';
+import readRelevantInfo from '../../../resources/helperFunctions/readingRelevantInfo.js';
+
+export default function CH128KLandingPage (props) {
+  let startInfoModified = readStartDateFile(startInfo);
+  let CH128LClassInfo = readRelevantInfo(relevantInfo);
+  return (
+    <>
+      <Navbar/>
+      <LandingPage
+        classColor='green-700'
+        classInfoLabs = {CH128LClassInfo.labs}
+        moreClassInfo = {CH128LClassInfo.moreInformation}
+        startInfo = {startInfoModified}
+      />
+      <Footer/>
+    </>
+  )
+}
