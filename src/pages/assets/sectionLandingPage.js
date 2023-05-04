17af55ef7b6e7bfad53b7223935f66067e7ac2db
Justin Stendara <jnstendara@gmail.com>
2023-05-03 22:20:48 -0500

    Fixed the styling on landing page

--- src/pages/assets/sectionLandingPage.js
+++ src/pages/assets/sectionLandingPage.js
@@ -6,20 +6,13 @@ import Link from 'next/link';
 export default function LandingPage (props) {
   return (
     <>
-      <div className='grid grid-cols-1 my-10'>
-      <div className="rounded overflow-hidden shadow-lg w-196 mx-auto mb-10" style={{backgroundColor:'#efefef'}}>
-        <div className={`font-bold text-2xl py-4 mb-3 px-6 text-center bg-${props.classColor} text-white`}>Syllabus</div>
-        <div className="px-6 py-4">
-          <Button
-            link=''
-            color={`bg-${props.classColor}`}
-            name={'Click here to see the syllabus'}
-          />
-        </div>
-      </div>
-
-
-        <div className="rounded overflow-hidden shadow-lg w-196 mx-auto mb-10" style={{backgroundColor:'#efefef'}}>
+      <div
+        className='grid grid-cols-1 my-10 mx-10 xl:mx-48 xl:mt-24 xl:mb-32 xl:grid-cols-2 '
+      >
+        <div
+          className="rounded overflow-hidden shadow-lg mx-auto col-span-1 h-min mb-10 min-w-lg max-w-full xl:col-span-1 xl:mb-0"
+          style={{backgroundColor:'#efefef'}}
+        >
           <div className={`font-bold text-2xl py-4 mb-3 text-center bg-${props.classColor} text-white px-6`}>Start Date</div>
           <div className="px-10 py-4 mx-auto align-center justify-center text-center">
             {Object.keys(props.startInfo).map((element, idx) => {
@@ -54,8 +47,12 @@ export default function LandingPage (props) {
           </div>
         </div>
 
-        <div className="rounded overflow-hidden shadow-lg w-96 xl:w-1/3 mx-auto" style={{backgroundColor:'#efefef'}}>
-          <div className={`font-bold text-2xl py-4 px-6 mb-3 text-center bg-${props.classColor} text-white`}>Relevant class information</div>
+        <div className='col-span-1'>
+          <div
+          className="rounded overflow-hidden shadow-lg w-96  mx-auto h-min xl:max-w-md"
+          style={{backgroundColor:'#efefef'}}
+        >
+          <div className={`font-bold text-2xl py-4 px-6 mb-3 text-center bg-${props.classColor} text-white`}>Important Documents</div>
           <div className="px-6 py-4 text-center">
             {props.moreClassInfo.map((element, idx) => {
               return (
@@ -65,6 +62,11 @@ export default function LandingPage (props) {
                 </>
               )
             })}
+            <Button
+                link=''
+                color={`bg-${props.classColor}`}
+                name={'Syllabus'}
+              />
 
             {props.classInfoLabs.map((obj) => {
               return (
@@ -80,6 +82,9 @@ export default function LandingPage (props) {
             })}
           </div>
         </div>
+
+        </div>
+
       </div>
     </>
   )
