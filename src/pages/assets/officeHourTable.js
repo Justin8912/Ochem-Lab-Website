2c50fffdfda6b4f3941fd508040c3e57fcc525d4
Justin Stendara <jnstendara@gmail.com>
2023-05-11 18:20:11 -0500

    Added borders to ta table

--- src/pages/assets/officeHourTable.js
+++ src/pages/assets/officeHourTable.js
@@ -5,16 +5,16 @@ export default function TAs (props) {
     <table className='text-center border-collapse mx-auto'>
       <tbody>
         <tr>
-          <th className='px-10'>TA</th>
-          <th className='px-10'>Email</th>
-          <th className='px-10'>Sections</th>
+          <th className='px-10 border-b'>TA</th>
+          <th className='px-10 border-b border-l border-r'>Email</th>
+          <th className='px-10 border-b'>Sections</th>
         </tr>
         {props.section.map(ta => {
           return (
             <tr key={uuidv4()}>
-              <td className='px-10'>{ta.name}</td>
-              <td className='px-10'>{ta.email}</td>
-              <td className='px-10'>{ta.sections}</td>
+              <td className='px-10 py-1'>{ta.name}</td>
+              <td className='px-10 border-l border-r py-1'>{ta.email}</td>
+              <td className='px-10 py-1'>{ta.sections}</td>
             </tr>
           )
         })}
