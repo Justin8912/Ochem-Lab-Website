7e66e21c8537e799a2172e4c99236f3506ace441
Justin Stendara <jnstendara@gmail.com>
2023-05-11 20:55:16 -0500

    Added classes, fixed colors, styled for all components - looks great!

--- src/pages/assets/ohTARendering.js
+++ src/pages/assets/ohTARendering.js
@@ -5,7 +5,7 @@ export default function CourseSection (props) {
   console.log(props)
   if (props.class) {
     return (
-      <div className={`relative shadow-lg text-center bg-white w-2/4 expanded-content max-h-fit mx-auto rounded-lg ease-linear bg-slate-50 transition-all duration-500 overflow-hidden mb-20`}>
+      <div className={`relative shadow-lg text-center bg-white w-2/5 expanded-content max-h-fit mx-auto rounded-lg ease-linear bg-slate-50 transition-all duration-500 overflow-hidden mb-20`}>
         <div onClick={props.accordian} class={`grid grid-cols-7 bg-${props.color}-50  py-5 px-10`} id={props.idHandle}>
           <svg className='col-span-1' xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16" id={props.idHandle}>
             <path fillRule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
@@ -13,17 +13,17 @@ export default function CourseSection (props) {
           <h3 id={props.idHandle} className={`font-bold text-xl text-center col-span-6`}>{props.title}</h3>
           </div>
           <div className='grid grid-cols-1'>
-            <div className='tableDisplay my-5'>
+            <div className='tableDisplay my-5 overflow-scroll'>
               <TATable section={props.info}/>
             </div>
-            <div className='items-end bg-red-50  py-5' id='ch128k'></div>
+            <div className={`items-end bg-${props.color}-50 py-5`} id={props.idHandle}></div>
           </div>
       </div>
     )
   } else {
     return (
-      <div className='text-center shadow-lg bg-white w-1/6 dropdown mx-auto rounded-lg bg-slate-50 transition-all duration-500 ease-linear mb-20'>
-        <div onClick={props.accordian} class={` bg-${props.color}-50 rounded-lg py-5 px-10`} id={props.idHandle}>
+      <div className='text-center shadow-lg bg-white w-1/5 xl:w-1/6 dropdown mx-auto rounded-lg bg-slate-50 transition-all duration-500 ease-linear mb-20'>
+        <div onClick={props.accordian} class={`grid grid-cols-7 bg-${props.color}-50 rounded-lg py-5 px-10`} id={props.idHandle}>
           <svg className='col-span-1' xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16" preserveAspectRatio="xMidYMin slice" id={props.idHandle}>
             <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
           </svg>
--- src/pages/officeHours.js
+++ src/pages/officeHours.js
@@ -36,7 +36,7 @@ export default function OH (props) {
         <p className='text-xl text-center mb-3'>Note: This page will be updated as more information become available!</p>
         <h2 className='font-bold text-2xl text-center mb-3'>Lab Coordinators</h2>
         <p className='text-xl text-center mb-3'>Dr. Conrad Fjetland – Office NHB 1.128, M 12-1, Th 10-11 in person</p>
-        <h2 className='text-l text-center mb-3'>Lab TAs e-mail addresses and Office Hours: (all TA office hours are via zoom, so check Canvas for the link)</h2>
+        <h2 className='text-l text-center mb-20'>Lab TAs e-mail addresses and Office Hours: (all TA office hours are via zoom, so check Canvas for the link)</h2>
           <div className='grid grid-cols-1'>
             <CourseInfo
               accordian={accordian}
