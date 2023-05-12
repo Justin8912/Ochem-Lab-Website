9fab03f4408767589e1278202345c6e23e32b8a1
Justin Stendara <jnstendara@gmail.com>
2023-05-11 20:37:51 -0500

    Need to figure out what is going on with the other two buttons that get rendered

--- src/pages/officeHours.js
+++ src/pages/officeHours.js
@@ -37,14 +37,36 @@ export default function OH (props) {
         <h2 className='font-bold text-2xl text-center mb-3'>Lab Coordinators</h2>
         <p className='text-xl text-center mb-3'>Dr. Conrad Fjetland – Office NHB 1.128, M 12-1, Th 10-11 in person</p>
         <h2 className='text-l text-center mb-3'>Lab TAs e-mail addresses and Office Hours: (all TA office hours are via zoom, so check Canvas for the link)</h2>
-          <CourseInfo
-            accordian={accordian}
-            class={ch128k}
-            info={info['128K']}
-            title={'CH128K TA Information'}
-          />
-          {/* <TATable section={info['128L']}/>
-          <TATable section={info['220C']}/> */}
+          <div className='grid grid-cols-1'>
+            <CourseInfo
+              accordian={accordian}
+              class={ch128k}
+              info={info['128K']}
+              title={'CH128K TA Information'}
+              color={'red'}
+              idHandle={'ch128k'}
+            />
+            {/* <TATable section={info['128L']}/>
+            <TATable section={info['220C']}/> */}
+            <CourseInfo
+              accordian = {accordian}
+              class={ch128l}
+              info={info['128L']}
+              title={'CH128L TA Information'}
+              color={'green'}
+              idHandle={'ch128l'}
+            />
+
+            <CourseInfo
+              accordian = {accordian}
+              class={ch220c}
+              info={info['220C']}
+              title={'CH220C TA Information'}
+              color={'blue'}
+              idHandle={'ch220c'}
+            />
+
+          </div>
 
 
           </div>
